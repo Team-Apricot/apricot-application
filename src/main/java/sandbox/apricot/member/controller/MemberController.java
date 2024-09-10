05c5e46064c02108bb8d989f834619036af14555
heejung <dap072055@naver.com>
2024-09-10 22:41:04 +0900

    :hammer: fix : 회원가입 mapping 추가

--- src/main/java/sandbox/apricot/member/controller/MemberController.java
+++ src/main/java/sandbox/apricot/member/controller/MemberController.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping
-
 public class MemberController {
+  @GetMapping("/signup")
+  public String signUp(Model model) {
+    return "/member/signup";
+  }
+
+  @GetMapping("/secondSignUp")
+  public String secondSignUp(Model model) {
+    return "/member/secondSignUp";
+  }
 
 
 }
