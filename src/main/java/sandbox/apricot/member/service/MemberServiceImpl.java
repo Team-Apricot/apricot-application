96472b41882a53378c4ec3ab0648d61e0a5da791
heonuk <gitshineit@gmail.com>
2024-09-14 20:10:33 +0900

    :sparkles: feat: 회원 가입 비즈니스 로직 개발

--- src/main/java/sandbox/apricot/member/service/MemberServiceImpl.java
+++ src/main/java/sandbox/apricot/member/service/MemberServiceImpl.java
@@ -7,8 +7,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sandbox.apricot.member.dto.request.MemberRegister;
-import sandbox.apricot.member.dto.response.MemberInfo;
+import sandbox.apricot.member.dto.request.MemberRegisterBasic;
+import sandbox.apricot.member.dto.request.MemberRegisterDetail;
 import sandbox.apricot.member.entity.Member;
 import sandbox.apricot.member.entity.MemberRole;
 import sandbox.apricot.member.exception.MemberBusinessException;
@@ -24,28 +24,31 @@ public class MemberServiceImpl implements MemberService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    @Transactional(readOnly = true)
-    public MemberInfo getMemberInfo(String email) {
-        log.info(" >>> [ 🔍 {} 회원을 조회 합니다. ]", email);
-        return MemberInfo.from(memberMapper.findByEmail(email)
-                        .orElseThrow(() -> new MemberBusinessException(MEMBER_NOT_FOUND)));
+    public void register(MemberRegisterBasic basicInfo, MemberRegisterDetail detailInfo) {
+        log.info(" >>> [ ✨ 회원 가입을 시도 합니다. ]");
+
+        // 회원 객체 생성
+        Member member = Member.builder()
+                .email(basicInfo.getEmail())
+                .nickName(basicInfo.getNickName())
+                .password(passwordEncoder.encode(basicInfo.getPassword()))
+                .ageRange(detailInfo.getAgeRange())
+                .gender(detailInfo.getGender())
+                .career(detailInfo.getCareer())
+                .marriedStatus(detailInfo.getMarriedStatus())
+                .numChild(Integer.parseInt(detailInfo.getNumChild()))
+                .memberRole(MemberRole.USER_ROLE)
+                .build();
+        memberMapper.insertMember(member);
+        log.info(" >>> [ ✨ 회원 가입 성공 ]");
     }
 
     @Override
-    public void insertMember(MemberRegister request) {
-        log.info(" >>> [ ✨ 회원 가입을 시도 합니다. ]");
-        Member member = Member.builder()
-                .email(request.getEmail())
-                .nickName(request.getNickName())
-                .password(passwordEncoder.encode(request.getPassword()))
-//                .ageRange(request.getAgeRange())
-//                .gender(request.getGender())
-//                .career(request.getCareer())
-//                .marriedStatus(request.getMarriedStatus())
-//                .numChild(request.getNumChild())
-                .memberRole(MemberRole.USER_ROLE)
-                .build();
-        log.info(" >>> [ ✨ Member: {} ]", member.toString());
-        memberMapper.insertMember(member);
+    @Transactional(readOnly = true)
+    public Long getMemberId(String email) {
+        Member member = memberMapper.findByEmail(email)
+                .orElseThrow(() -> new MemberBusinessException(MEMBER_NOT_FOUND));
+        return member.getMemberId();
     }
+
 }
