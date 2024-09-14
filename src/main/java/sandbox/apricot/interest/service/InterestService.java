eb83bb8b4b8dd29ced607aef0fb25a2d1540e9ce
heonuk <gitshineit@gmail.com>
2024-09-14 20:09:41 +0900

    :hammer: fix: 관심사 등록 비즈니스 로직 수정

--- src/main/java/sandbox/apricot/interest/service/InterestService.java
+++ src/main/java/sandbox/apricot/interest/service/InterestService.java
@@ -1,10 +1,9 @@
 package sandbox.apricot.interest.service;
 
 import java.util.List;
-import sandbox.apricot.interest.dto.request.InterestRegister;
 
 public interface InterestService {
 
-    void register(List<InterestRegister> request, Long memberId);
+    void register(List<String> request, Long memberId);
 
 }
--- src/main/java/sandbox/apricot/interest/service/InterestServiceImpl.java
+++ src/main/java/sandbox/apricot/interest/service/InterestServiceImpl.java
@@ -1,6 +1,7 @@
 package sandbox.apricot.interest.service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -18,9 +19,13 @@ public class InterestServiceImpl implements InterestService {
     private final InterestMapper interestMapper;
 
     @Override
-    public void register(List<InterestRegister> request, Long memberId) {
+    public void register(List<String> request, Long memberId) {
         log.info(" >>> [ ✨ 관심사 등록을 시도 합니다. ]");
-        for(InterestRegister category : request) {
+        List<InterestRegister> interests = IntStream.range(0, request.size())
+                .mapToObj(idx -> new InterestRegister(request.get(idx), idx + 1))
+                .toList();
+
+        for(InterestRegister category : interests) {
             Interest interest = Interest.builder()
                     .memberId(memberId)
                     .categoryId(category.getCategoryId())
