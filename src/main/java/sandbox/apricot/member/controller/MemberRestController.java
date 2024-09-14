38485c9b98cf6c0d26117b787ea115d833f36f2d
heonuk <gitshineit@gmail.com>
2024-09-14 20:12:05 +0900

    :sparkles: feat: 회원 가입 RestAPI 개발

--- src/main/java/sandbox/apricot/member/controller/MemberRestController.java
+++ src/main/java/sandbox/apricot/member/controller/MemberRestController.java
@@ -1,16 +1,23 @@
 package sandbox.apricot.member.controller;
 
+import static org.springframework.http.HttpStatus.*;
+
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import java.util.List;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.apricot.common.response.ApiResponse;
-import sandbox.apricot.member.dto.request.MemberRegister;
+import sandbox.apricot.interest.dto.request.InterestRegister;
+import sandbox.apricot.interest.service.InterestService;
+import sandbox.apricot.member.dto.request.MemberRegisterBasic;
+import sandbox.apricot.member.dto.request.MemberRegisterDetail;
 import sandbox.apricot.member.service.MemberService;
 
 @RestController
@@ -20,15 +27,45 @@ import sandbox.apricot.member.service.MemberService;
 public class MemberRestController {
 
     private final MemberService memberService;
+    private final InterestService interestService;
+    private final HttpSession session;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse> registerMember (@RequestBody @Valid MemberRegister request) {
-        memberService.insertMember(request);
+    /**
+     * 회원 가입을 위해 필요한 기본 정보를 세션에 저장
+     *
+     * @param request - email, password, nickName
+     */
+    @PostMapping("/register-basic")
+    public ResponseEntity<ApiResponse> saveMember(@RequestBody @Valid MemberRegisterBasic request) {
+        session.setAttribute("basicInfo", request);
         return ResponseEntity.ok().body(
                 ApiResponse.successResponse(
-                        HttpStatus.OK,
+                        OK,
+                        "기본 정보가 세션에 저장 되었습니다."
+                )
+        );
+    }
+
+    /**
+     * 회원 가입 - 회원 등록, 관심사 등록
+     *
+     * @param request - ageRange, gender, career, marriedStatus, numChild, interests
+     */
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> registerMember(@RequestBody @Valid MemberRegisterDetail request) {
+        MemberRegisterBasic basicInfo = (MemberRegisterBasic) session.getAttribute("basicInfo");
+        memberService.register(basicInfo, request);
+
+        Long memberId = memberService.getMemberId(basicInfo.getEmail());
+        interestService.register(request.getInterests(), memberId);
+
+        session.removeAttribute("basicInfo"); // 기본 정보 삭제
+        return ResponseEntity.ok().body(
+                ApiResponse.successResponse(
+                        OK,
                         "성공적으로 회원가입을 완료하였습니다."
-                ));
+                )
+        );
     }
 
 }
