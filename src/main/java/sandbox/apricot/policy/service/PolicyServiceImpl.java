4bffd7e67e56565bcdd9da813050226907ff5a0e
heonuk <gitshineit@gmail.com>
2024-09-23 14:41:45 +0900

    :fire: hotfix: 중복 코드 제거

--- src/main/java/sandbox/apricot/policy/service/PolicyServiceImpl.java
+++ src/main/java/sandbox/apricot/policy/service/PolicyServiceImpl.java
@@ -100,39 +100,4 @@ public class PolicyServiceImpl implements PolicyService {
         return policyDao.selectParticipationPolicy(policyCode, categoryCode);
     }
 
-    // 전체 정책 조회
-    @Override
-    public List<PolicyDTO> selectAllPolicy(String policyCode, String categoryCode) throws Exception {
-        return policyDao.selectAllPolicy(policyCode, categoryCode);
-    }
-
-
-    public List<PolicyDTO> selectJobsPolicy(String policyCode, String categoryCode, String districtCode) throws Exception {
-        return policyDao.selectJobsPolicy(policyCode, categoryCode, districtCode);
-    }
-
-    // 주거 정책 조회
-    @Override
-    public List<PolicyDTO> selectHousingPolicy(String policyCode, String categoryCode) throws Exception {
-        return policyDao.selectHousingPolicy(policyCode, categoryCode);
-    }
-
-    // 교육 정책 조회
-    @Override
-    public List<PolicyDTO> selectEducationPolicy(String policyCode, String categoryCode) throws Exception {
-        return policyDao.selectEducationPolicy(policyCode, categoryCode);
-    }
-
-    // 복지 정책 조회
-    @Override
-    public List<PolicyDTO> selectWelfarePolicy(String policyCode, String categoryCode) throws Exception {
-        return policyDao.selectWelfarePolicy(policyCode, categoryCode);
-    }
-
-    // 참여권리 정책 조회
-    @Override
-    public List<PolicyDTO> selectParticipationPolicy(String policyCode, String categoryCode) throws Exception {
-        return policyDao.selectParticipationPolicy(policyCode, categoryCode);
-    }
-
 }
