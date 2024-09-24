5a0fee0a114c807b9af668e4b418112c830fcc7a
heejung <dap072055@naver.com>
2024-09-24 22:13:19 +0900

    :bug: fix: 정책 검색 메퍼 추가

--- src/main/java/sandbox/apricot/policy/mapper/PolicyMapper.java
+++ src/main/java/sandbox/apricot/policy/mapper/PolicyMapper.java
@@ -3,10 +3,13 @@ package sandbox.apricot.policy.mapper;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import sandbox.apricot.policy.dto.response.DistrictPolicy;
+import sandbox.apricot.policy.dto.response.PolicyInfo;
 
 @Mapper
 public interface PolicyMapper {
 
     List<DistrictPolicy> getPolicyCountByDistrict();
+
+    List<PolicyInfo> findPolicy(String searchName);
 
 }
