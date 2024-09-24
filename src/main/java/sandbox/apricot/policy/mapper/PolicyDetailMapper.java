86e84912042a69858a11b99948d63f4cb7094618
heonuk <gitshineit@gmail.com>
2024-09-24 19:03:22 +0900

    :fire: remove: 사용하지 않는 클래스 삭제

--- src/main/java/sandbox/apricot/policy/mapper/PolicyDetailMapper.java
+++ src/main/java/sandbox/apricot/policy/mapper/PolicyDetailMapper.java
@@ -1,6 +1,5 @@
 package sandbox.apricot.policy.mapper;
 
-import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import sandbox.apricot.policy.dto.PolicyDetailDTO;
@@ -8,7 +7,6 @@ import sandbox.apricot.policy.dto.PolicyDetailDTO;
 @Mapper
 public interface PolicyDetailMapper {
 
-    List<PolicyDetailDTO> getPolicyDetails();
-
     PolicyDetailDTO getPolicyDetailsByPolicyCode(@Param("policyCode") String policyCode);
+
 }
