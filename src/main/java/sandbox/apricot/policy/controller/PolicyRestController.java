a9bb8c7dc94ba024a2e4091a51f8dae4c32c9852
heonuk <gitshineit@gmail.com>
2024-09-24 19:08:46 +0900

    :recycle: refactor: [페이지네이션] RestAPI 개발 및 area.jsp 페이지 호출 로직 수정

--- src/main/java/sandbox/apricot/policy/controller/PolicyRestController.java
+++ src/main/java/sandbox/apricot/policy/controller/PolicyRestController.java
@@ -6,13 +6,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import sandbox.apricot.common.response.ApiResponse;
-import sandbox.apricot.policy.dto.request.PolicyPagination;
 import sandbox.apricot.policy.dto.response.District;
 import sandbox.apricot.policy.dto.response.DistrictPolicies;
 import sandbox.apricot.policy.dto.response.DistrictPolicy;
@@ -39,10 +37,13 @@ public class PolicyRestController {
         return ApiResponse.successResponse(OK, "성공적으로 지역구 정보를 조회하였습니다.", data);
     }
 
-    @GetMapping("/all")
-    public ApiResponse<DistrictPolicies> getAllPoliciesByDistrictCode(
-            @RequestBody PolicyPagination request) {
-        DistrictPolicies data = policyService.getAllDistrictPolicies(request.toService());
+    @GetMapping("/district-list")
+    public ApiResponse<DistrictPolicies> getDistrictPolicies(
+            @RequestParam(value = "categoryCode", required = false) String categoryCode,
+            @RequestParam("districtCode") String districtCode,
+            @RequestParam(value = "page", defaultValue = "1") int page
+        ) {
+        DistrictPolicies data = policyService.getDistrictPolicies(districtCode, categoryCode, page);
         return ApiResponse.successResponse(OK, "성공적으로 지역구 혜택을 조회하였습니다.", data);
     }
 
